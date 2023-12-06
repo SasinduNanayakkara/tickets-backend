936cc830d7d8de782f1048978699fea23c961f7d
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-12-07 04:04:04 +0530

    Implement logger with morgan middleware

--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,9 +1,51 @@
-import { devLogger } from "./DevLogger";
-import {Logger} from 'winston'
-let logger: Logger | null = null;
+// import { devLogger } from "./DevLogger";
+// import {Logger} from 'winston'
+// let logger: Logger | null = null;
 
-logger = devLogger();
-if (process.env.NODE_ENV === 'DEV') {
+// logger = devLogger();
+// if (process.env.NODE_ENV === 'DEV') {
+// }
+
+// export default logger;
+
+
+import winston from "winston";
+
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    debug: 4
 }
+
+const level = () => {
+    const env = process.env.NODE_ENV || 'development'
+    const isDevelopment = env === 'development'
+    return isDevelopment ? 'debug' : 'warn'
+  }
+
+const format = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    winston.format.printf(
+        (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+    ),
+)
+
+const transports = [
+    new winston.transports.Console(),
+    new winston.transports.File({
+        filename: 'logs/error/log',
+        level: 'error'
+    }),
+    new winston.transports.File({filename: 'logs/all.log'}),
+];
+
+const logger = winston.createLogger({
+    level: level(),
+    levels,
+    format,
+    transports
+});
 
 export default logger;
