ca4a428589d399ede9ab4d0f177a5db12c87767a
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-12-08 03:00:01 +0530

    Implement tokens

--- src/Services/Auth.service.ts
+++ src/Services/Auth.service.ts
@@ -1,18 +1,33 @@
 import jwt from 'jsonwebtoken';
 import { v4 as uuidv4 } from 'uuid'; 
+import { ACCESS_TOKEN_TYPE } from '../Utils/Constants';
+import { client } from '../Database/redisDB';
 
-export const generateAccessTokenService = (userId = "", roles = [""]) => {
-    const secret = process.env.ACCESS_TOKEN_SECRET || "";
+export const generateAccessTokenService = async (userId = "", roles = [""], type: number) => {
+    const secret = type === ACCESS_TOKEN_TYPE ? process.env.ACCESS_TOKEN_SECRET || "" : process.env.REFRESH_TOKEN_SECRET || "";
+    const jti = uuidv4();
     const option = { 
-        expiresIn: '1h',
+        expiresIn: type === ACCESS_TOKEN_TYPE ? '20m' : '1y',
         issuer: "http://localhost:5000/api/v1",
         audience: userId
     };
     const payload = {
         sub: userId,
-        jti: uuidv4(),
+        jti: jti,
         roles: roles
     }
 
-    return jwt.sign(payload, secret, option);
+    const token = jwt.sign(payload, secret, option);
+    if (type === ACCESS_TOKEN_TYPE) {
+        if (userId) {
+            await client.SET(userId, token);
+        }
+        else {
+            await client.SET(jti, token);
+        }
+    }
+    else {
+        await client.SET(userId + "-" + 'refresh', token);
+    }
+    return token;
 }
