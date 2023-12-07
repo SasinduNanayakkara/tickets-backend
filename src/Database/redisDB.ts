e7fad91b82510c390052b8da63a7f7ba78700ab5
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-12-08 03:01:24 +0530

    Implement redis DB

--- src/Database/redisDB.ts
+++ src/Database/redisDB.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'redis';
 
 export const client = createClient({
-        password: process.env.REDIS_PASSWORD || "Yse9jncRRUdjLxlPLYQIAsE4QRUrE8CP",
+        password: process.env.REDIS_PASSWORD || "",
         socket: {
-            host: process.env.REDIS_HOST || "redis-12163.c267.us-east-1-4.ec2.cloud.redislabs.com",
+            host: process.env.REDIS_HOST || "",
             port: 12163
         }
     });
