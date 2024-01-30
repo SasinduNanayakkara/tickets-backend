44c442b8caeea09617d6b93a6ce97cf435d58434
SasinduNanayakkara <it20625016@my.sliit.lk>
2024-01-31 03:18:08 +0530

    Implement createAdmin controller

--- src/Routes/User.routes.ts
+++ src/Routes/User.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, getUserByUserId } from "../Controllers/Users.controller";
+import { createAdmin, createUser, getUserByUserId } from "../Controllers/Users.controller";
 import { verifyAccessToken } from "../Utils/jwt";
 
 const router = Router();
 
 router.post('/', verifyAccessToken, createUser);
 router.get('/:id', getUserByUserId);
+router.post('/admin', createAdmin);
 
 export default router;
