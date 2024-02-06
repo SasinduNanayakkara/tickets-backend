ec9d1282f97d63b61767ad9872741d9660ec2d78
SasinduNanayakkara <it20625016@my.sliit.lk>
2024-02-07 03:13:39 +0530

    Implement getTicketByEventId API

--- src/Routes/Ticket.routes.ts
+++ src/Routes/Ticket.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyAccessToken } from "../Utils/jwt";
-import { createDigitalTicketController, getTicketDetailsByIdController, getTicketsByUserIdController } from "../Controllers/Ticket.Controller";
+import { createDigitalTicketController, getTicketDetailsByIdController, getTicketsByUserIdController, getTicketsDetailsByEventId } from "../Controllers/Ticket.Controller";
 
 const router = Router();
 
 router.get("/:id", verifyAccessToken, getTicketDetailsByIdController);
 router.post("/download", verifyAccessToken, createDigitalTicketController);
 router.get("/user/:id", verifyAccessToken, getTicketsByUserIdController);
+router.get("/event/:id", verifyAccessToken, getTicketsDetailsByEventId);
 
 export default router;
